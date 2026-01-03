0a6a4d3af252855caeb5552fccd7ccba2ead1111
Leonardo <leo.davi.207@gmail.com>
2026-01-02 22:41:47 -0500

    Subiendo los demás productos (derivados y cafes)

---
+++ scripts/cafe.js
@@ -0,0 +1,119 @@
+window.addEventListener('DOMContentLoaded', function() {
+    new Glider(document.querySelector('.f1_lista'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: {
+            prev: '.f1_left',
+            next: '.f1_right'
+        },
+        responsive: [
+        {
+        breakpoint: 500,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 700,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {breakpoint: 900,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 1040,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+        }
+        }
+        ]
+    });
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+    new Glider(document.querySelector('.f2_lista'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: {
+            prev: '.f2_left',
+            next: '.f2_right'
+        },
+        responsive: [
+        {
+        breakpoint: 500,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 700,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {breakpoint: 900,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 1040,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+        }
+        }
+        ]
+    });
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+    new Glider(document.querySelector('.f3_lista'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: {
+            prev: '.f3_left',
+            next: '.f3_right'
+        },
+        responsive: [
+        {
+        breakpoint: 500,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 700,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {breakpoint: 900,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 1040,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+        }
+        }
+        ]
+    });
+})
---
+++ scripts/lacteos_despensa.js
@@ -0,0 +1,199 @@
+window.addEventListener('DOMContentLoaded', function() {
+    new Glider(document.querySelector('.d1_lista'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: {
+            prev: '.d1_left',
+            next: '.d1_right'
+        },
+        responsive: [
+        {
+        breakpoint: 500,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 700,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {breakpoint: 900,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 1040,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+        }
+        }
+        ]
+    });
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+    new Glider(document.querySelector('.d2_lista'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: {
+            prev: '.d2_left',
+            next: '.d2_right'
+        },
+        responsive: [
+        {
+        breakpoint: 500,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 700,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {breakpoint: 900,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 1040,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+        }
+        }
+        ]
+    });
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+    new Glider(document.querySelector('.d3_lista'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: {
+            prev: '.d3_left',
+            next: '.d3_right'
+        },
+        responsive: [
+        {
+        breakpoint: 500,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 700,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {breakpoint: 900,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 1040,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+        }
+        }
+        ]
+    });
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+    new Glider(document.querySelector('.d4_lista'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: {
+            prev: '.d4_left',
+            next: '.d4_right'
+        },
+        responsive: [
+        {
+        breakpoint: 500,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 700,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {breakpoint: 900,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 1040,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+        }
+        }
+        ]
+    });
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+    new Glider(document.querySelector('.d5_lista'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: {
+            prev: '.d5_left',
+            next: '.d5_right'
+        },
+        responsive: [
+        {
+        breakpoint: 500,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 700,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+        }
+        },
+        {breakpoint: 900,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+        }
+        },
+        {
+        breakpoint: 1040,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+        }
+        }
+        ]
+    });
+})
